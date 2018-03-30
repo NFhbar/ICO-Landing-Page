5e738a007ceb3c9b4fe5376a5e4322be514329f7
NFhbar <frega.nicolas@gmail.com>
2018-03-30 14:29:24 -0700

    fixed typos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
 
     # Returns the full title on a per-page basis.
     def full_title(page_title = '')
-      base_title = "BIG"
+      base_title = "OSHIE"
       if page_title.empty?
         base_title
       else
